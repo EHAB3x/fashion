c39e4899c9dd2e2f60d4f6bb59854a03c9fcc533
EHAB3x <ehabmah6969@gmail.com>
2024-04-17 00:06:27 +0200

    using hash router instead of browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { register } from 'swiper/element/bundle';
 import { Provider } from 'react-redux';
 import { store } from './RTK/store.js';
 register();
 ReactDOM.createRoot(document.getElementById('root')).render(  
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 )
